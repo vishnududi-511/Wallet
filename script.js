12df3fa17815b98bfdffe5f942d76a344232a3af
vishnududi-511 <dudivishnu4@gmail.com>
2024-02-23 10:40:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let toggle_box = document.querySelector(".toggle-ball")
+
+toggle_box.addEventListener("click", function () {
+    toggle_box.classList.toggle("toggle-ball-right")
+})
+
+
+let menuicons = document.querySelector("#menubar")
+let navlist = document.querySelector(".nav-items")
+
+menuicons.addEventListener("click", () => {
+    navlist.classList.toggle("show");
+})
